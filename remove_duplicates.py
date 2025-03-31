90e2cece28ebaf9b4ac3092104dcbbd88f9d65cc
parsashu <a.zshahidi91@gmail.com>
2025-03-31 18:35:44 +0330

    remove dups

---
+++ remove_duplicates.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+df = pd.read_csv("datasets/merged.tsv", sep="\t")
+total_rows = len(df)
+
+df_no_duplicates = df.drop_duplicates(keep="last")
+rows_after_dedup = len(df_no_duplicates)
+duplicates_removed = total_rows - rows_after_dedup
+
+df_no_duplicates.to_csv("datasets/merged_no_duplicates.tsv", sep="\t", index=False)
+
+print(f"Total rows before deduplication: {total_rows}")
+print(f"Total rows after deduplication: {rows_after_dedup}")
+print(f"Number of duplicate rows removed: {duplicates_removed}")
+
+
