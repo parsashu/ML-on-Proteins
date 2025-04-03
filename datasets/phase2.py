eabbaa7e987e02e20bfbb6de1810d3dcae6143e5
parsashu <a.zshahidi91@gmail.com>
2025-04-03 20:56:19 +0330

    normalized phase 2

--- datasets/phase2.py
+++ datasets/phase2.py
@@ -1,43 +1,33 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
+import numpy as np
+from sklearn.preprocessing import StandardScaler
 
+df_train = pd.read_csv("datasets/phase2/train/X_train.csv")
+df_test = pd.read_csv("datasets/phase2/test/X_test.csv")
 
-df = pd.read_csv("datasets/features.csv")
+df_train_normalized = df_train.copy()
+df_test_normalized = df_test.copy()
 
-features = [
+numeric_cols = [
     "ASA",
     "pH",
     "T_(C)",
-    "Coil",
-    "Helix",
-    "Sheet",
-    "Turn",
     "Tm_(C)",
     "m_(kcal/mol/M)",
     "Cm_(M)",
     "∆G_H2O_(kcal/mol)",
 ]
 
-# Generate a list of embedding feature names
-embed_features = [f"embed_{i}" for i in range(320)]
+scaler = StandardScaler()
 
-features_df = df[features].copy()
-features_df = features_df.fillna(-1000)
-
-labels_df = df[embed_features].copy()
-labels_df = labels_df.fillna(0)
-
-train_features, test_features, train_labels, test_labels = train_test_split(
-    features_df, labels_df, test_size=0.1, random_state=42
+df_train_normalized = pd.DataFrame(
+    scaler.fit_transform(df_train_normalized), columns=df_train.columns
+)
+df_test_normalized = pd.DataFrame(
+    scaler.transform(df_test_normalized), columns=df_test.columns
 )
 
-train_features.to_csv("datasets/phase2/train/X_train.csv", index=False)
-test_features.to_csv("datasets/phase2/test/X_test.csv", index=False)
-train_labels.to_csv("datasets/phase2/train/y_train.csv", index=False)
-train_labels.to_csv("datasets/phase2/train/y_train.csv.gz", index=False, compression="gzip")
-test_labels.to_csv("datasets/phase2/test/y_test.csv", index=False)
-test_labels.to_csv("datasets/phase2/test/y_test.csv.gz", index=False, compression="gzip")
+df_train_normalized.to_csv("datasets/phase2/train/X_train_normalized.csv", index=False)
+df_test_normalized.to_csv("datasets/phase2/test/X_test_normalized.csv", index=False)
 
-print(
-    "Features and labels split and saved to datasets/phase2/train and datasets/phase2/test directories"
-)
+print("Datasets normalized and saved successfully")
