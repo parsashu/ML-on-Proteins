bba0b67e33dbf799b6b6dc340e2772439302d01f
parsashu <a.zshahidi91@gmail.com>
2025-04-03 20:30:52 +0330

    finished phase 2 split

--- datasets/phase2.py
+++ datasets/phase2.py
@@ -2,15 +2,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 
-df = pd.read_csv("datasets/protein_dataset.tsv", sep="\t")
-
-df["SEC_STR_ENCODED"] = df["SEC_STR_ENCODED"].apply(
-    lambda x: eval(x) if isinstance(x, str) else x
-)
-df["Coil"] = df["SEC_STR_ENCODED"].apply(lambda x: x[0])
-df["Helix"] = df["SEC_STR_ENCODED"].apply(lambda x: x[1])
-df["Sheet"] = df["SEC_STR_ENCODED"].apply(lambda x: x[2])
-df["Turn"] = df["SEC_STR_ENCODED"].apply(lambda x: x[3])
+df = pd.read_csv("datasets/features.csv")
 
 features = [
     "ASA",
@@ -26,13 +18,26 @@ features = [
     "∆G_H2O_(kcal/mol)",
 ]
 
+# Generate a list of embedding feature names
+embed_features = [f"embed_{i}" for i in range(320)]
+
 features_df = df[features].copy()
 features_df = features_df.fillna(-1000)
 
+labels_df = df[embed_features].copy()
+labels_df = labels_df.fillna(0)
 
-train_df, test_df = train_test_split(features_df, test_size=0.1, random_state=42)
+train_features, test_features, train_labels, test_labels = train_test_split(
+    features_df, labels_df, test_size=0.1, random_state=42
+)
 
-train_df.to_csv("datasets/phase2/train/X_train.csv", index=False)
-test_df.to_csv("datasets/phase2/test/X_test.csv", index=False)
+train_features.to_csv("datasets/phase2/train/X_train.csv", index=False)
+test_features.to_csv("datasets/phase2/test/X_test.csv", index=False)
+train_labels.to_csv("datasets/phase2/train/y_train.csv", index=False)
+train_labels.to_csv("datasets/phase2/train/y_train.csv.gz", index=False, compression="gzip")
+test_labels.to_csv("datasets/phase2/test/y_test.csv", index=False)
+test_labels.to_csv("datasets/phase2/test/y_test.csv.gz", index=False, compression="gzip")
 
-print("Features split and saved to train_features.csv and test_features.csv")
+print(
+    "Features and labels split and saved to datasets/phase2/train and datasets/phase2/test directories"
+)
